77791aea0e66a1904180da30052dc7b667d01310
Thiago Nunes <thiagonunes.tns@gmail.com>
2021-10-21 21:47:19 -0300

    Save partial spam list (delta hour) as csv

--- script.py
+++ script.py
@@ -17,6 +17,7 @@ import json
 import requests
 import pandas as pd
 import csv
+from datetime import datetime
 
 # +
 URL_YOUMAIL_API_LIST = "https://dataapi.youmail.com/directory/spammers/v2/partial/since/"
@@ -25,6 +26,7 @@ URL_YOUMAIL_API_PARTIAL_HOUR = "https://dataapi.youmail.com/api/v3/spammerlist/p
 
 CSV_FOLDER = "files"
 YOUMAIL_FULL_FILENAME = "spam-number-file.csv"
+YOUMAIL_PART_FILENAME = "spam-number-file_"
 
 
 # -
@@ -110,4 +112,52 @@ def save_youmail_full():
         
     return
 
-save_youmail_full()
+
+#mock
+def partial_number_operation(number, spam_score, fraud_probability, TCPA_fraud_probability):
+    '''
+    with open(CSV_FOLDER + "/" + YOUMAIL_FULL_FILENAME, 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if number == row[0]:
+                return 'M'
+    return 'A'
+    '''
+    return '-'
+
+
+#get the delta 
+def save_this_hour_partial_spam_list():
+    
+    base = datetime.today()
+    hour = base.strftime('%Y%m%dT%H0000Z')
+
+    #get the partial file
+    diff = get_youmail_partial_list(hour)
+
+    #transform investigationReasons data
+    for d in diff['phoneNumbers']:
+        if d['investigationReasons']:
+            for i in d['investigationReasons']:
+                d[i['name']] = i['certainty']
+    
+    #prepare dataframe
+    df = pd.DataFrame(diff['phoneNumbers'])
+    df.drop('investigationReasons', axis=1, inplace=True)
+    df.columns = ['Number', 'SpamScore', 'FraudProbability', 'TCPAFraudProbability']
+    
+    #calculate Operation field based on last full list
+    df['Operation'] = df.apply(lambda row: partial_number_operation(row['Number'], 
+                                                                    row['SpamScore'], 
+                                                                    row['FraudProbability'], 
+                                                                    row['TCPAFraudProbability']), axis=1)
+    
+    #spam-number-file_yyyymmddhh:mm:ss
+    time_file = base.strftime('%Y%m%d%H:00:00')
+    
+    df.to_csv(CSV_FOLDER + "/" + YOUMAIL_PART_FILENAME + hour + ".csv", index=False)
+    
+    return
+
+
+save_this_hour_partial_spam_list()
