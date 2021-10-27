09d9cc40da5313f402ec883fe3d8c3692ccd0878
Thiago Sousa <thiagonunes.tns@gmail.com>
2021-10-26 22:55:46 -0300

    fix

--- script.py
+++ script.py
@@ -350,7 +350,7 @@ if __name__ == "__main__":
     
     hour_to_filename = datetime.now().strftime('%Y%m%d%H%M%S')
 
-    prefix = sys.argv[1:][0] if 'FULL' in sys.argv[1:] or 'NETCHANGE' in sys.argv[1:]  else 'INVALID_PARAMETER'
+    prefix = sys.argv[1:][0] if 'FULL' in sys.argv[1:] or 'NETCHANGE' in sys.argv[1:] or 'CLEAN' in sys.argv[1:] else 'INVALID_PARAMETER'
 
     logging.basicConfig(level=logging.INFO, 
                         format='%(asctime)s - %(levelname)s - %(message)s',
